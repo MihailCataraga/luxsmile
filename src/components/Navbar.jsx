7796cbb355518cace9a52a7a76f200508745c5dc
Mihai <cataraga.95@gmail.com>
2024-02-15 19:59:00 +0200

    90%

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -135,7 +135,7 @@ export default function Navbar(props) {
                                     <nav>
                                         <NavLink onClick={scrollToTop} to='/'>Acasa</NavLink>
                                         <NavLink onClick={scrollToTop} to='/despreNoi'>Despre Noi</NavLink>
-                                        <div className='servicesBoxMini' onClick={(e) => { e.stopPropagation(); setServiceMini(!serviceMini) }}>
+                                        <div className={`servicesBox ${serviceColor}`} onClick={(e) => { e.stopPropagation(); setServiceMini(!serviceMini) }}>
                                             Servicii
                                             {serviceMini && <div className='services'>
                                                 <nav>
